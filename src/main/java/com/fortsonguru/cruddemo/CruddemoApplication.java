d1e5cb1e341490dd513f4e7690ed65ebe68d2e1b
Dano <danocolombo@gmail.com>
2025-03-11 05:50:03 -0400

    added function to create multiple students. Tested.

--- src/main/java/com/fortsonguru/cruddemo/CruddemoApplication.java
+++ src/main/java/com/fortsonguru/cruddemo/CruddemoApplication.java
@@ -17,8 +17,23 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+//			createStudent(studentDAO);
+			createMultipleStudents(studentDAO);
 		};
+	}
+
+	private void createMultipleStudents(StudentDAO studentDAO) {
+		// create a student object
+		System.out.println("Creating 3 new student objects");
+		Student tempStudent1 = new Student("Larry", "Lizard", "ll@email.com");
+		Student tempStudent2 = new Student("Julie", "Wentz", "jw@email.com");
+		Student tempStudent3 = new Student("Anna", "Miller", "am@email.com");
+
+		// save the student object
+		System.out.println("Saving the students");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 	}
 
 	private void createStudent(StudentDAO studentDAO) {
